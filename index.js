743ed9b498a27497bf0ff362c9492e57098d5631
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-07-08 09:30:29 -0400

    adding env variables and dotenvdependency

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const app = express(); 
+require('dotenv').config()
 
 app.get('/', (req,res) => {
     res.send('Hello World'); 
 });
 
 app.get('/api/courses', (req,res)=>{
-    res.send([])
+    res.send([
+        {
+            id: 1,
+            name: 'REST API'
+        },
+        {
+            id: 2,
+            name: 'SEND GRID'
+        },
+        {
+            id: 3,
+            name: 'NUXTJS'
+        },
+        {
+            id: 4,
+            name: 'DEPLOYMENT'
+        },
+        {
+            id: 4,
+            name: 'nodemon'
+        },
+    ])
 });
 
-app.listen(1500, () => console.log('Listening on port 1500...'))
+// PORT
+const port = process.env.PORT || 1500;
+app.listen(port, () => console.log(`Listening on port ${port} ...`))
